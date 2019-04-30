b66fe5246ad886fcaf2ce15048476898b3420c50
Eduardo Espinoza <42099005+tttedu304@users.noreply.github.com>
2019-04-30 18:36:30 -0500

    Added fetch

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,39 +1,43 @@
 /*
 Functions to fetch to Estreck API
 */
+const fetch = require('node-fetch');
 exports.getCodes = async() => {
     let toReturn = "";
     fetch('https://estreck.ml/codes')
-        .then(function(res) {
+        .then((res) => {
         return res.json();
         })
-        .then(function(json) {
+        .then((json) => {
             toReturn = json;
-        });
+        })
+        .catch(err => console.log(err));
     return toReturn;
 };
 
 exports.getUnverifiedCodes = async() => {
     let toReturn = "";
     fetch('https://estreck.ml/nocodes')
-        .then(function(res) {
+        .then((res) => {
             return res.json();
         })
-        .then(function(json) {
+        .then((json) => {
             toReturn = json;
-        });
+        })
+        .catch(err => console.log(err));
     return toReturn;
 };
 
 exports.fetchCode = async(search) => {
     let toReturn = "";
     fetch(`https://estreck.ml/codes/search/${search}`)
-        .then(function(res) {
+        .then((res) => {
             return res.json();
         })
-        .then(function(json) {
+        .then((json) => {
             toReturn = json;
-        });
+        })
+        .catch(err => console.log(err));
     return toReturn;
 };
 
