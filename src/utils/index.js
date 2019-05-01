254e8e4d6ebdf3135d248baf35504e2e388938ab
Eduardo Espinoza <42099005+tttedu304@users.noreply.github.com>
2019-04-30 19:33:36 -0500

    Fixed function json error

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,17 +4,17 @@ Functions to fetch to Estreck API
 const fetch = require('node-fetch');
 exports.getCodes = async() => {
     let res = await fetch('https://estreck.ml/codes');
-    return res.json;
+    return res.json();
 };
 
 exports.getUnverifiedCodes = async() => {
     let res = await fetch('https://estreck.ml/nocodes');
-    return res.json;
+    return res.json();
 };
 
 exports.fetchCode = async(search) => {
     let res = await fetch(`https://estreck.ml/codes/search/${search}`);
-    return res.json;
+    return res.json();
 };
 
 exports.addCode = async() => {
